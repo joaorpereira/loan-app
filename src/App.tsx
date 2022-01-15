168dcfe600ff1160a66bd3371c8a9b2f7f2c519d
joaorpereira <jprp17@live.com>
2022-01-15 16:06:07 -0300

    feature: change material ui version

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router } from "react-router-dom";
 import Routes from "routes";
-import { ThemeProvider, CssBaseline } from "@mui/material";
+import { ThemeProvider, CssBaseline } from "@material-ui/core";
 import { theme } from "styles/theme";
 
 function App() {
--- src/styles/theme.tsx
+++ src/styles/theme.tsx
@@ -1,5 +1,5 @@
-import { createTheme } from "@mui/material/styles";
-import { green, purple } from "@mui/material/colors";
+import { createTheme } from "@material-ui/core";
+import { green, purple } from "@material-ui/core/colors";
 
 const theme = createTheme({
 	palette: {
