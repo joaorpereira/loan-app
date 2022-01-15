1232cdf4444318c8c82a2be6fa6d618d735b5996
joaorpereira <jprp17@live.com>
2022-01-15 18:27:21 -0300

    feafture: add decorator theme to storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,17 @@
+import React from "react";
+
+import { addDecorator } from "@storybook/react";
+import { ThemeProvider } from "@material-ui/core/styles";
+import { theme } from "../src/styles/theme";
+
+addDecorator((story) => <ThemeProvider theme={theme}>{story()}</ThemeProvider>);
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
-}
+	actions: { argTypesRegex: "^on[A-Z].*" },
+	controls: {
+		matchers: {
+			color: /(background|color)$/i,
+			date: /Date$/
+		}
+	}
+};
--- src/styles/theme.tsx
+++ src/styles/theme.tsx
@@ -1,13 +1,9 @@
 import { createTheme } from "@material-ui/core";
-import { green, purple } from "@material-ui/core/colors";
 
 const theme = createTheme({
 	palette: {
 		primary: {
-			main: purple[500]
-		},
-		secondary: {
-			main: green[500]
+			main: "#408cea"
 		}
 	},
 	typography: {
