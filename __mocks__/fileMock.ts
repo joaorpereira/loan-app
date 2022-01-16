f392c77d0b9b9eb1a084e7cb56afd85bf76d9f9d
joaorpereira <jprp17@live.com>
2022-01-16 18:29:16 -0300

    feature: mock images

---
+++ __mocks__/fileMock.ts
@@ -0,0 +1,3 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import Logo from "assets/check.svg";
+import Heart from "assets/savings.svg";
--- jest.config.js
+++ jest.config.js
@@ -7,5 +7,9 @@ module.exports = {
 		"!src/**/*.stories.@(js|jsx|ts|tsx)"
 	],
 	setupFilesAfterEnv: ["<rootDir>/.jest/setup.ts"],
-	moduleDirectories: ['node_modules', '.']
+	moduleDirectories: ['node_modules', '.'],
+	moduleNameMapper: {
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+      "<rootDir>/__mocks__/fileMock.ts"
+  }
 };
