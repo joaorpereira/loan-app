981314eeec9f0ca7010a9a417c5042d175a3617c
joaorpereira <jprp17@live.com>
2022-01-16 20:17:29 -0300

    feature: change other option for base url in api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-	baseURL: process.env.REACT_APP_URL || "http://localhost:3003",
+	baseURL:
+		process.env.REACT_APP_URL ||
+		"https://clutch-interview-service.herokuapp.com/",
 	headers: {
 		"Content-Type": "application/json"
 	}
