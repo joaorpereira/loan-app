1b66f451eca1ee3f18e6236278316bcb673fac0a
joaorpereira <jprp17@live.com>
2022-01-16 20:17:13 -0300

    feature: create renderWithRouter helper

--- src/tests/helpers.tsx
+++ src/tests/helpers.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 import { render, RenderResult } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "@material-ui/core";
 import { theme } from "styles/theme";
 
 export const renderWithTheme = (children: React.ReactNode): RenderResult =>
 	render(<ThemeProvider theme={theme}>{children}</ThemeProvider>);
+
+export const renderWithRouter = (children: React.ReactNode): RenderResult =>
+	render(
+		<BrowserRouter>
+			<ThemeProvider theme={theme}>{children}</ThemeProvider>
+		</BrowserRouter>
+	);
