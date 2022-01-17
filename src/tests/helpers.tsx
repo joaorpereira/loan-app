3d9a564e7ff0f4f68f57db88b03fd94d94efa35e
joaorpereira <jprp17@live.com>
2022-01-16 23:48:18 -0300

    refactor: create provider with theme and router for testing

--- src/tests/helpers.tsx
+++ src/tests/helpers.tsx
@@ -4,10 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "@material-ui/core";
 import { theme } from "styles/theme";
 
-export const renderWithTheme = (children: React.ReactNode): RenderResult =>
-	render(<ThemeProvider theme={theme}>{children}</ThemeProvider>);
-
-export const renderWithRouter = (children: React.ReactNode): RenderResult =>
+export const renderProvider = (children: React.ReactNode): RenderResult =>
 	render(
 		<BrowserRouter>
 			<ThemeProvider theme={theme}>{children}</ThemeProvider>
